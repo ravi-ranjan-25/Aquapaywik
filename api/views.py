ff977365d073e677fddb5962df2a5d24856dd830
Ravi Ranjan <singhranjanrr@gmail.com>
2019-10-31 20:21:56 +0530

    4th deploy

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from api.models import houseDetails,area,areaQuantity,quality,userConsumption,Complain,tax,wallet
+from api.models import houseDetails,area,areaQuantity,quality,userConsumption,Complain,Tax,wallet
 from api.forms import UserForm,areaForm,houseForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
