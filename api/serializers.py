e4f234c0a82ea5e581f33935fc43562d33bceb56
Ravi Ranjan <singhranjanrr@gmail.com>
2019-11-01 00:18:50 +0530

    4th deploy

--- api/serializers.py
+++ api/serializers.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 class userSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['pk','username','first_name','last_name']
+        fields = ['pk','username','first_name','last_name','email']
